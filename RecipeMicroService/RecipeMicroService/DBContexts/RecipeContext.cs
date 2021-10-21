6f693c1a4d9035e16f24e95b429d0b6f33e6a0ba
kevin hofstede <kmhofstede@gmail.com>
2021-10-21 12:39:59 +0200

    init

---
+++ RecipeMicroService/RecipeMicroService/DBContexts/RecipeContext.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.EntityFrameworkCore;
+using RecipeMicroService.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RecipeMicroService.DBContexts
+{
+    public class RecipeContext : DbContext
+    {
+        public RecipeContext(DbContextOptions<RecipeContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Recipe> Recipes { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Recipe>().HasData(
+          new Recipe
+          {
+              Id = 1,
+              Name = "Boboti",
+              Description = "iets met boontjes",
+          },
+          new Recipe
+          {
+              Id = 2,
+              Name = "Boerenkool",
+              Description = "ouderwets hollands",
+          },
+          new Recipe
+          {
+              Id = 3,
+              Name = "Schnitzel",
+              Description = "Gewoon simpel",
+          }
+      );
+        }
+    }
+}
---
+++ RecipeMicroService/RecipeMicroService/Repository/IRecipeRepository.cs
@@ -0,0 +1,15 @@
+﻿using RecipeMicroService.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RecipeMicroService.Repository
+{
+    interface IRecipeRepository
+    {
+        IEnumerable<Recipe> GetRecipes();
+        Recipe GetRecipeByID(int RecipeId);
+
+    }
+}
